c8536479b76de345fb73847e62ba039b2717eaae
ArthurMeloG <arthurdemelo2001@gmail.com>
2021-03-12 10:04:22 -0300

    (#43) Realizado Login automatico

--- src/index.js
+++ src/index.js
@@ -9,3 +9,35 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+/* PEGAR BOTAO CLOSE MILESTONE*/
+/*const puppeteer = require('puppeteer');
+
+async function closeCurrentmilestone(){
+  const browser = await puppeteer.launch({headless: false });
+  const page = await browser.newPage();
+  await page.goto('https://github.com);
+  await page.screenshot({ path: 'example.png' });
+
+  await browser.close();
+
+}*/
+
+const puppeteer = require('puppeteer');
+/*LOGIN NA CONTA DO GITHUB, ESSA CONTA É APENAS DE TESTES,FUTURAMENTE IMPLEMENTAR VIA VARIÁVEIS DE AMBIENTE*/
+(async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  /*ENTRA NA AREA DE LOGIN E PASSA O USER E SENHA*/
+  await page.goto('https://github.com/login');
+
+  const inputLogin = 'PullBotTestes' /*CONTA TESTE PARA ENTRAR GITHUB*/
+  const inputPassword = 'passocaepamonha'
+
+  await page.type('[name="login"]', inputLogin) 
+  await page.type('[name="password"]', inputPassword)
+  await page.click('[name="commit"]')
+  
+
+  await browser.close();
+})();
