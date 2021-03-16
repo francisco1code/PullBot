9b42949e3cc54b9c62daea533dcb09732b21dda5
ArthurMeloG <arthurdemelo2001@gmail.com>
2021-03-16 11:42:24 -0300

    (#30) (#44) (#45) recebimento de valores para passar para a API, owner e repo

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@
 
 document.addEventListener("click", function(e) {
 
-    
-console.log("URL atual: " + window.location.pathname);
-
-var currentUrl = String(window.location.pathname)
-var urlParts = currentUrl.split("/")
-console.log(urlParts)
+    var currentUrl = String(window.location.pathname)
+    var urlParts = currentUrl.split("/")
+    console.log(urlParts)
 
     if(urlParts[3] == "milestones" && urlParts[0] == ""){
+    const $owner = urlParts[2]
+    const $repo = urlParts[3]
 
     var text = e.path[0].classList
-    console.log(e)
     
     if(String(text) == 'btn-link'){
     window.alert("closed")
