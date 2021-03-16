e9a71a6047d04225f7c1d98ed15244a94f229c49
ArthurMeloG <arthurdemelo2001@gmail.com>
2021-03-16 11:38:13 -0300

    (#30) (#44) (#45) Criterio de funcionamento somente se estiver na parte de milestone do GitHub

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
+
+
 document.addEventListener("click", function(e) {
 
     
 console.log("URL atual: " + window.location.pathname);
 
-var text = e.path[0].classList
-    console.log(e)
+var currentUrl = String(window.location.pathname)
+var urlParts = currentUrl.split("/")
+console.log(urlParts)
 
+    if(urlParts[3] == "milestones" && urlParts[0] == ""){
+
+    var text = e.path[0].classList
+    console.log(e)
+    
     if(String(text) == 'btn-link'){
     window.alert("closed")
     }
+}
+
+
+
 });
 
 /*const $btnClose = document.querySelector('.d-inline-block.mr-2 button')
