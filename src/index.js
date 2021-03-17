1527fd542e5c773a7badca6309b6d30a1bc839a8
ArthurMeloG <arthurdemelo2001@gmail.com>
2021-03-17 07:49:02 -0400

    (#44) (#45) (#47) Resolovido problema com botao close e reopen, implementado funçao de request

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
-
-
-document.addEventListener("click", function(e) {
-
-    var currentUrl = String(window.location.pathname)
-    var urlParts = currentUrl.split("/")
-    console.log(urlParts)
-
-    if(urlParts[3] == "milestones" && urlParts[0] == ""){
-    const $owner = urlParts[2]
-    const $repo = urlParts[3]
-
-    var text = e.path[0].classList
-    
-    if(String(text) == 'btn-link'){
-    window.alert("closed")
-    }
-}
-
-
-
-});
-
 /*const $btnClose = document.querySelector('.d-inline-block.mr-2 button')
 /*/
+
+document.addEventListener("click", function (e) {
+    var currentUrl = String(window.location.href);
+    var urlParts = currentUrl.split("/");
+    
+    //VALIDA ENTRADA EM GITHUB MILESTONES
+    if (urlParts[5] == "milestones" && urlParts[2] == "github.com") {
+        
+        var closeMilestoneButton = document.querySelector(".d-inline-block.mr-2 .btn-link");
+        //VALIDA FECHAMENTO DE MILESTONE
+        if (e.path[0] == closeMilestoneButton &&
+            closeMilestoneButton.textContent.toLowerCase() == "close") {
+               
+            var milestoneName = document.querySelector(".milestone-title-link").innerText;
+            // window.alert("Closed: " + milestoneName);
+
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    // Typical action to be performed when the document is ready:
+                    alert(xhttp.responseText);
+                }
+            };
+            xhttp.open("GET", "https://github.com/fga-eps-mds/PullBot/milestones");
+            xhttp.send();
+
+        }
+    }
+});
