432a41d75d89fa66206bad3fbc8575adf20b8180
ArthurMeloG <arthurdemelo2001@gmail.com>
2021-03-18 19:37:24 -0400

    (#44) (#45) Organizando o codigo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-/*const $btnClose = document.querySelector('.d-inline-block.mr-2 button')
-/*/
-
+/*PEGAR EVENTO DE CLICK NA PAGINA*/
 document.addEventListener("click", function (e) {
     var currentUrl = String(window.location.href);
     var urlParts = currentUrl.split("/");
@@ -20,40 +18,44 @@ document.addEventListener("click", function (e) {
 
        
     //VALIDA FECHAMENTO DE MILESTONE
-        if (e.path[0] == closeMilestoneButton &&
-            closeMilestoneButton.textContent.toLowerCase() == "close") {
-                var base = document.querySelectorAll("form.d-inline-block.mr-2").action
-                console.log(base)
-            var milestoneName = document.querySelector(".milestone-title-link").innerText;
+    if (e.path[0] == closeMilestoneButton &&
+        closeMilestoneButton.textContent.toLowerCase() == "close") {
+        var base = document.querySelectorAll("form.d-inline-block.mr-2").action
+        console.log(base)
+        var milestoneName = document.querySelector(".milestone-title-link").innerText;
              
 
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4 && this.status == 200) {
-                    // Typical action to be performed when the document is ready:
-                    alert(xhttp.responseText);
+        var xhttp = new XMLHttpRequest();
+         xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                 // Typical action to be performed when the document is ready:
+                alert(xhttp.responseText);
                 }
             };
-            xhttp.open("GET", "https://api.github.com/repos/fga-eps-mds/PullBot");
-            xhttp.send();
+
+        xhttp.open("GET", "https://api.github.com/repos/fga-eps-mds/PullBot");
+        xhttp.send();
 
         }
     }
 });
+/*FUNÇOES A DEFINIREM OS VALORES A SEREM PASSADOS PARA API*/
 
-
+/*GET DONO DO REPOSITORIO*/
 function getOwner(valuesAPI){
-
     var owner = valuesAPI[3];
     console.log(owner)
     return owner;
-
 }
+
+/*GET  REPOSITORIO*/
 function getRepositori(valuesAPI){
     var repo = valuesAPI[4]
     console.log(repo)
     return repo;
 }
+
+/*GET NUMERO DA MILESTONE A SER FECHADA*/
  function getNumberMilestone(valuesAPI){
     var milestoneNumber = valuesAPI[6]
     console.log(milestoneNumber)
