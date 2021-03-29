fcfed048f53d72a520a6293ce5499eeeab36f958
ArthurMeloG <arthurdemelo2001@gmail.com>
2021-03-29 10:49:24 -0400

    Refatoraçao

--- src/index.js
+++ src/index.js
@@ -1,73 +1,97 @@
-// SALVA TOKEN DO USUÁRIO
-$input_token = document.querySelector("#token")
-$btn_submit = document.querySelector("#btn_submit")
-
-$btn_submit.addEventListener('click', () => {
-    let token = $input_token.value
-    window.alert(token)
-})
-
-/*PEGAR EVENTO DE CLICK NA PAGINA*/
 document.addEventListener("click", function (e) {
     var currentUrl = String(window.location.href);
     var urlParts = currentUrl.split("/");
     
     //VALIDA ENTRADA EM GITHUB MILESTONES
     if (urlParts[5] == "milestones" && urlParts[2] == "github.com") {
-
+  
     //BOTOES    
     var closeMilestoneButton = document.querySelector(".d-inline-block.mr-2 .btn-link");
     var getFormAction = document.querySelectorAll('form[class="d-inline-block mr-2"]')[0].action;
-
-    //VALORES A SEREM PASSADOS PARA API
-    var valuesAPI = String(getFormAction).split("/");
-    const $owner = getOwner(valuesAPI);
-    const $repo = getRepositori(valuesAPI);
-    const $NumberMilestone = getNumberMilestone(valuesAPI);
-
-       
+  
+  
     //VALIDA FECHAMENTO DE MILESTONE
     if (e.path[0] == closeMilestoneButton &&
         closeMilestoneButton.textContent.toLowerCase() == "close") {
-        var base = document.querySelectorAll("form.d-inline-block.mr-2").action
-        console.log(base)
+        var base = document.querySelectorAll("form.d-inline-block.mr-2").action;
+        console.log(base);
         var milestoneName = document.querySelector(".milestone-title-link").innerText;
-             
-
-        var xhttp = new XMLHttpRequest();
-         xhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                 // Typical action to be performed when the document is ready:
-                alert(xhttp.responseText);
-                }
-            };
-
-        xhttp.open("GET", "https://api.github.com/repos/fga-eps-mds/PullBot");
-        xhttp.send();
-
+        
+      // PEGAR VALORES DE BRANCHS
+    const currentBrnach =  window.prompt("Qual branch para merge ?");
+    const recieveBranch = window.prompt("Qual branch destino ?");    
+    
+      var data = JSON.stringify({
+            "title": "Amazing new feature",
+            "body": "Please pull this in!",
+            "head": currentBrnach,
+            "base": recieveBranch
+          });
+          
+        //VALORES A SEREM PASSADOS PARA API
+      var valuesAPI = String(getFormAction).split("/");
+      const $owner = getOwner(valuesAPI);
+      const $repo = getRepositori(valuesAPI);
+      const $NumberMilestone = getNumberMilestone(valuesAPI);
+  
+    
+  
+        /*  xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+              console.log(this.responseText);
+            }
+          });
+          
+          xhr.open("POST", "https://api.github.com/repos/" + $owner + "/" + $repo + "/pulls");
+          xhr.setRequestHeader("accept", "application/vnd.github.v3+json");
+          xhr.setRequestHeader("content-type", "application/json");
+          xhr.setRequestHeader("authorization","Bearer 5775e10c014ae90cbdabadbe9b2595ad35437559" );
+  
+          xhr.send(data);*/
         }
     }
-});
-/*FUNÇOES A DEFINIREM OS VALORES A SEREM PASSADOS PARA API*/
-
-/*GET DONO DO REPOSITORIO*/
-function getOwner(valuesAPI){
+  });
+  
+  
+  
+  
+  function loginDevice(codeUser){
+  
+    var currentUrl = String(window.location.href);
+    var urlParts = currentUrl.split("/");
+    if(urlParts[2] == "github.com" && urlParts[3] == "login" && urlParts[4] == "device"){
+    document.getElementById("user-code-0").value = (codeUser.subStr(0, 0));
+    document.getElementById("user-code-1").value = (codeUser.subStr(1, 1));
+    document.getElementById("user-code-2").value = (codeUser.subStr(2, 2));
+    document.getElementById("user-code-3").value = (codeUser.subStr(3, 3));
+    document.getElementById("user-code-4").value = (codeUser.subStr(5, 5));
+    document.getElementById("user-code-5").value = (codeUser.subStr(6, 6));
+    document.getElementById("user-code-6").value = (codeUser.subStr(7, 7));
+    document.getElementById("user-code-7").value = (codeUser.subStr(8, 8));
+    
+  }
+  }
+  
+  /*FUNÇOES A DEFINIREM OS VALORES A SEREM PASSADOS PARA API*/
+  
+  /*GET DONO DO REPOSITORIO*/
+  function getOwner(valuesAPI){
     var owner = valuesAPI[3];
     console.log(owner)
     return owner;
-}
-
-/*GET  REPOSITORIO*/
-function getRepositori(valuesAPI){
+  }
+  
+  /*GET  REPOSITORIO*/
+  function getRepositori(valuesAPI){
     var repo = valuesAPI[4]
     console.log(repo)
     return repo;
-}
-
-/*GET NUMERO DA MILESTONE A SER FECHADA*/
- function getNumberMilestone(valuesAPI){
+  }
+  
+  /*GET NUMERO DA MILESTONE A SER FECHADA*/
+  function getNumberMilestone(valuesAPI){
     var milestoneNumber = valuesAPI[6]
     console.log(milestoneNumber)
     return milestoneNumber;
- }
+  }
    
