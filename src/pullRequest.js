18d143e9348946a211f87c540f84c6cd812bb826
LorrayneCardozo <lorraynecardozo15@gmail.com>
2021-04-10 12:58:56 -0300

    exluindo tags (#86)

--- src/pullRequest.js
+++ src/pullRequest.js
@@ -101,9 +101,6 @@ export function criarPullRequest() {
             </div>
         </div>
     </div>
-
-    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     `
     const body = document.body || document.getElementsByTagName("body")[0] || document.documentElement;
     body.insertBefore(modal, body.lastChild);
