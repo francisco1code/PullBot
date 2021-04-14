f1d128358c35b3a2c083a9ab1bded9e4557e19f0
zpeniel09 <zpeniel09@gmail.com>
2021-04-14 18:10:12 -0300

    (#97) Adição dos labls para o chartJS

--- CHART/js/app.js
+++ CHART/js/app.js
@@ -2,7 +2,7 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Black'],
     },    
     
 });    
