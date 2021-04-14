880718b32e2606a7e783a8f1b73625ea1d0ba6d2
zpeniel09 <zpeniel09@gmail.com>
2021-04-14 18:15:49 -0300

    (#97) Adição do background Color

--- CHART/js/app.js
+++ CHART/js/app.js
@@ -3,6 +3,20 @@ var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Black'],
+        datasets: [{
+            label: '# Gráfico',
+            data: [12, 19, 3, 5, 2, 3, 40],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(153, 102, 255, 0.2)'
+            ],
+            
+        }]
     },    
     
 });    
