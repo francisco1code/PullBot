4fa2266912b00ea56bbdd62d464bb7acf6783c1b
LorrayneCardozo <lorraynecardozo15@gmail.com>
2021-04-17 20:23:49 -0300

    alterações no relatório #98

--- src/modules/relatorio.js
+++ src/modules/relatorio.js
@@ -3,42 +3,55 @@
 export function criarRelatorio(){
   
     var xhr = new XMLHttpRequest();
-    
-    
+    const owner = localStorage.getItem('owner');
+    const repo = localStorage.getItem('repo');
+
     xhr.addEventListener("readystatechange", function() {
       if(this.readyState === 4) {
-        var recebeContribuintes = JSON.parse(this.responseText);
-        montarRelatorio(recebeContribuintes);
+        var contribuintes = JSON.parse(this.responseText);
+        montarRelatorio(contribuintes);
   
     }});
     
-    xhr.open("GET", "https://api.github.com/repos/fga-eps-mds/PullBot/contributors");
+    xhr.open("GET", `https://api.github.com/repos/${owner}/${repo}/contributors`);
     xhr.send();
 }
 
 function montarRelatorio(contribuintes) {
 
-    var relatorio = `# Relatorio de Desenvolvimento \n## 1. Commits \n| | Contribuinte | Quantidade de commits \n|:-:|:-:|:-:| \n`;
+    const milestoneName = localStorage.getItem('milestoneName');
+    var relatorio = `# Relatorio de desenvolvimento da milestone: ${milestoneName} \n## 1. Ranking de Contribuições \n| | Contribuinte | Quantidade de Contribuições \n|:-:|:-:|:-:| \n`;
 
-// COMMITS
-    for(var i = 0; i < 7; i++) {
-        relatorio += `| ${i+1} | ${contribuintes[i].login} |  | \n`;
+// CONTRIBUIÇÕES
+    var totalContribuicoes = 0;
+    for(var i = 0; i < contribuintes.length; i++) {
+        relatorio += `| ${i+1}º | ${contribuintes[i].login} | ${contribuintes[i].contributions} | \n`;
+        totalContribuicoes += contribuintes[i].contributions;
     }
 
-    relatorio += `| Total |  |  | \n`;
+    relatorio += `|  | Total | ${totalContribuicoes} | \n`;
+    relatorio += `## 2. Commits \n| Contribuinte | Quantidade de Commits | \n|:-:|:-:| \n`;
+    
+// COMMITS
+    for(i = 0; i < contribuintes.length; i++) {
+        relatorio += `| ${contribuintes[i].login} |  | \n`;
+    }
 
-    relatorio += `## 2. Issues \nContribuinte | Issues abertas | Issues fechadas \n|:-:|:-:|:-:| \n`;
+relatorio += `| Total |  |  | \n`;
+
+
+    relatorio += `## 3. Issues \n| Contribuinte | Issues abertas | Issues fechadas \n|:-:|:-:|:-:| \n`;
 
 // ISSUES
-    for(i = 0; i < 7; i++) {
+    for(i = 0; i < contribuintes.length; i++) {
         relatorio += `| ${contribuintes[i].login} |  |  | \n`;
     }
 
     relatorio += `| Total |  |  | \n`;
-    relatorio += `## 3. Pull Requests \nContribuinte | Pull Requests abertos | Pull Requests fechados \n|:-:|:-:|:-:| \n`;
+    relatorio += `## 4. Pull Requests \n| Contribuinte | Pull Requests abertos | Pull Requests fechados \n|:-:|:-:|:-:| \n`;
 
 // PULL REQUESTS
-for(i = 0; i < 7; i++) {
+for(i = 0; i < contribuintes.length; i++) {
     relatorio += `| ${contribuintes[i].login} |  |  | \n`;
 }
 
@@ -46,3 +59,4 @@ relatorio += `| Total |  |  | \n`;
 
 console.log(relatorio);
 }
+
