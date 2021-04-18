d9740c3211c41b7569123b0d41684b9b473ad39d
ArthurMeloG <arthurdemelo2001@gmail.com>
2021-04-18 09:13:26 -0300

    (#96) Chart.js para a pasta chart

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -18,4 +18,16 @@ document.addEventListener('DOMContentLoaded', function()
     });
 
     document.getElementById('test').textContent = 'SUCCEED';
+
+  
+        /*Get image of canvas element*/
+        var url_base64jp = document.getElementById("lineChart").toDataURL("image/pdf");
+        /*get download button (tag: <a></a>) */
+        var a =  document.getElementById("download");
+        /*insert chart image url to download button (tag: <a></a>) */
+        a.href = url_base64jp;
+    
+   
 });
+
+     
