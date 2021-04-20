6c9470b5e8fe643e74ef360818411eb303947e57
Eliás Yousef <eliasyousef00@gmail.com>
2021-04-20 11:59:32 -0300

    (#96) (#99) colocando a pdfmake dentro do repositorio

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,33 +1,26 @@
 import {GraficoPessoal} from '../CHART/app.js';
+//import {pdfmakeMin} from './pdfmakeMin.js';
 
-const $btn_download = document.querySelector(".btn-download")
+const $btn_download = document.querySelector("#btn-download")
 const $canvas = document.querySelector("#canvas")
 
 document.addEventListener('DOMContentLoaded', function(){
     GraficoPessoal()
-    document.getElementById('test').textContent = 'SUCCEED';
-
-  
-        /*Get image of canvas element*/
-        var url_base64jp = document.getElementById("chart").toDataURL("image/pdf");
-        /*get download button (tag: <a></a>) */
-        var a =  document.getElementById("download");
-        /*insert chart image url to download button (tag: <a></a>) */
-        a.href = url_base64jp;
 });
 
-document.addEventListener('click', () => {
-    var data = $canvas.toDataURL('image/png', 1.0)
-    console.log(data)
+$btn_download.addEventListener('click', () => {
+    
+    var dataURL = $canvas.toDataURL('image/png', 1.0);
+    //console.log(data)
 
     var docDefinition = {
         content: [
             {
-                image: data
+                image: dataURL
             }
         ]
     }
-    pdfMake.createPdf(docDefinition).download();
+    pdfMake.createPdf(docDefinition).open();
 
 })
 
