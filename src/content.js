4c7cfd1cb22e1e007177c50b845c5bfdce7fa382
LorrayneCardozo <lorraynecardozo15@gmail.com>
2021-04-22 14:30:46 -0300

    corrigindo erro #98

--- src/content.js
+++ src/content.js
@@ -9,4 +9,4 @@ const scriptFileSaver = document.createElement('script');
     scriptFileSaver.setAttribute("type", "module");
     scriptFileSaver.setAttribute("src", chrome.runtime.getURL('FileSaver.js'));
     const headFileSaver = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
-    headFileSaver.insertBefore(script1, headFileSaver.lastChild);    
+    headFileSaver.insertBefore(scriptFileSaver, headFileSaver.lastChild);    
