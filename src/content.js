68b34ddbea77c6c8338ec95422ce9bd59c8473a4
LorrayneCardozo <lorraynecardozo15@gmail.com>
2021-04-22 14:29:54 -0300

    defininfo FileSaver.js como um módulo #98

--- src/content.js
+++ src/content.js
@@ -5,4 +5,8 @@ const script = document.createElement('script');
     const head = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
     head.insertBefore(script, head.lastChild);
 
-    
+const scriptFileSaver = document.createElement('script');
+    scriptFileSaver.setAttribute("type", "module");
+    scriptFileSaver.setAttribute("src", chrome.runtime.getURL('FileSaver.js'));
+    const headFileSaver = document.head || document.getElementsByTagName("head")[0] || document.documentElement;
+    headFileSaver.insertBefore(script1, headFileSaver.lastChild);    
