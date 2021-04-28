c2c1f764663f2132aeba29cf018e09c0896700f9
Eliás Yousef <eliasyousef00@gmail.com>
2021-04-28 16:02:54 -0300

    (#96) (#99) remoção do evento de click no btn download pdf

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -11,24 +11,7 @@ chrome.storage.sync.get(['key'], function(result) {
   contribuinteRepositorio(dados[0], dados[1] , $owner,  $repo )
 });
 
-const $btn_download = document.querySelector("#btn-download")
-const $canvas = document.querySelector("#canvas")
 
-$btn_download.addEventListener('click', () => {
-    
-    var dataURL = $canvas.toDataURL('image/png', 1.0);
-    //console.log(data)
-
-    var docDefinition = {
-        content: [
-            {
-                image: dataURL
-            }
-        ]
-    }
-    pdfMake.createPdf(docDefinition).open();
-
-})
 
 
      
