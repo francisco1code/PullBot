eba18fa0dd85a47bf35b987659b1800d70a18ed9
LorrayneCardozo <lorraynecardozo15@gmail.com>
2021-05-03 20:34:54 -0300

    (#107) corrigindo problemas no teste

--- src/testes/relatorio.test.js
+++ src/testes/relatorio.test.js
@@ -1,28 +1,6 @@
 import criarRelatorio from '../modules/relatorio';
 import fileSaver from '../libraries/filesaver';
 
-
-// MOCK DA CLASSE XMLHttpRequest
-const mockXHR = {
-  open: jest.fn(),
-  send: jest.fn(),
-  readyState: 4,
-  addEventListener: jest.fn(),
-  setRequestHeader: jest.fn(),
-  responseText: [
-    {
-    "teste": 'teste',
-    "teste1": 'teste1',
-  },
-  {
-    "teste2": 'teste',
-    "teste3": 'teste'
-  }
-]
-};
-const oldXMLHttpRequest = window.XMLHttpRequest;
-window.XMLHttpRequest = jest.fn(() => mockXHR);
-
 var objetoJson = [
   [{
   "teste": 'teste',
@@ -31,15 +9,36 @@ var objetoJson = [
 [{
   "teste2": 'teste',
   "teste3": 'teste'
-}]
-]
+}]]
+
+
+// MOCK DA CLASSE XMLHttpRequest
+const mockXHR = {
+  addEventListener: jest.fn(),
+  setRequestHeader: jest.fn(),
+  open: jest.fn(),
+  send: jest.fn(() => {
+    try {
+      var quantidadeDeChamadas = mockXHR.addEventListener.mock.calls.length;
+      if(quantidadeDeChamadas > 0) {
+        mockXHR.addEventListener.mock.calls[quantidadeDeChamadas-1][1]();
+        return;
+      }
+      
+    } catch (e) {}
+    
+    }
+  ),
+  readyState: 4,
+  responseText: objetoJson
+};
+window.XMLHttpRequest = jest.fn(() => mockXHR);
 
 // MOCK DE UM OBJETO JSON
 JSON.parse = jest.fn().mockReturnValue(objetoJson)
 
 // MOCK DO MÉTODO FILESAVER
 jest.mock('../libraries/filesaver')
-
 
 test("calls fileSaver()", () => {
     criarRelatorio();
