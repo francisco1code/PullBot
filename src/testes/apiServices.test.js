16c1bba1958ded04c7513ec0ba43859d637d02c5
LorrayneCardozo <lorraynecardozo15@gmail.com>
2021-05-05 16:20:20 -0300

    (#107) criando primeiro teste de apiServices.js

---
+++ src/testes/apiServices.test.js
@@ -0,0 +1,47 @@
+import {CodigoDevicePost} from '../modules/apiServices';
+
+// MOCK DA CLASSE XMLHttpRequest
+const mockXHR = {
+addEventListener: jest.fn(),
+setRequestHeader: jest.fn(),
+open: jest.fn(),
+send: jest.fn(() => {
+    try {
+        var quantidadeDeChamadas = mockXHR.addEventListener.mock.calls.length;
+        if(quantidadeDeChamadas > 0) {
+            mockXHR.addEventListener.mock.calls[quantidadeDeChamadas-1][1]();
+            return;
+        }
+    } catch (e) {}
+}),
+readyState: 4,
+responseText: 'device_code=00000000000000000000000000000&expires_in=000&interval=5&user_code=0000-ABCD&verification_uri=https%3A%2F%2Fgithub.com%2Flogin%2Fdevice'
+};
+window.XMLHttpRequest = jest.fn(() => mockXHR);
+
+
+// MOCK DO OBJETO Document
+const mockDocument = {
+    value: null
+}  
+window.document.getElementById = jest.fn(() => mockDocument)
+
+
+CodigoDevicePost();
+
+test("call loginDevice()", () => {
+    
+// TESTANDO SE CodigoDevicePost() EXECUTOU O MÉTODO loginDevice() CORRETAMENTE
+    // TESTANDO SE document.getElementById() FOI EXECUTADO 8 VEZES (uma vez para cada elemento do vetor codeUser)
+    expect(document.getElementById).toHaveBeenCalledTimes(8);
+
+    // TESTANDO SE OS 8 ELEMENTOS DO VETOR codeUser FORAM PASSADOS CORRETAMENTE
+    expect(document.getElementById.mock.calls[0][0]).toBe('user-code-0')
+    expect(document.getElementById.mock.calls[1][0]).toBe('user-code-1')
+    expect(document.getElementById.mock.calls[2][0]).toBe('user-code-2')
+    expect(document.getElementById.mock.calls[3][0]).toBe('user-code-3')
+    expect(document.getElementById.mock.calls[4][0]).toBe('user-code-5')
+    expect(document.getElementById.mock.calls[5][0]).toBe('user-code-6')
+    expect(document.getElementById.mock.calls[6][0]).toBe('user-code-7')
+    expect(document.getElementById.mock.calls[7][0]).toBe('user-code-8')
+});
