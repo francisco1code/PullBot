fe2b1f1364335c8f52f1ef3fa70f6476f1a90f19
LorrayneCardozo <lorraynecardozo15@gmail.com>
2021-05-05 17:50:26 -0300

    (#107) teste do método ConfirmaLoginUsuário()

--- src/testes/apiServices.test.js
+++ src/testes/apiServices.test.js
@@ -1,21 +1,22 @@
 import {CodigoDevicePost} from '../modules/apiServices';
+import {ConfirmaLoginContaUsuario} from '../modules/apiServices';
 
 // MOCK DA CLASSE XMLHttpRequest
 const mockXHR = {
-addEventListener: jest.fn(),
-setRequestHeader: jest.fn(),
-open: jest.fn(),
-send: jest.fn(() => {
-    try {
-        var quantidadeDeChamadas = mockXHR.addEventListener.mock.calls.length;
-        if(quantidadeDeChamadas > 0) {
-            mockXHR.addEventListener.mock.calls[quantidadeDeChamadas-1][1]();
-            return;
-        }
-    } catch (e) {}
-}),
-readyState: 4,
-responseText: 'device_code=00000000000000000000000000000&expires_in=000&interval=5&user_code=0000-ABCD&verification_uri=https%3A%2F%2Fgithub.com%2Flogin%2Fdevice'
+    addEventListener: jest.fn(),
+    setRequestHeader: jest.fn(),
+    open: jest.fn(),
+    send: jest.fn(() => {
+        try {
+            var quantidadeDeChamadas = mockXHR.addEventListener.mock.calls.length;
+            if(quantidadeDeChamadas > 0) {
+                mockXHR.addEventListener.mock.calls[quantidadeDeChamadas-1][1]();
+                return;
+            }
+        } catch (e) {}
+    }),
+    readyState: 4,
+    responseText: 'device_code=CHAVE_DO_DEVICE_CODE123&expires_in=000&interval=5&user_code=0000-ABCD&verification_uri=https%3A%2F%2Fgithub.com%2Flogin%2Fdevice'
 };
 window.XMLHttpRequest = jest.fn(() => mockXHR);
 
@@ -24,14 +25,22 @@ window.XMLHttpRequest = jest.fn(() => mockXHR);
 const mockDocument = {
     value: null
 }  
-window.document.getElementById = jest.fn(() => mockDocument)
+window.document.getElementById = jest.fn(() => mockDocument);
+
+
+// MOCK DO localStorage
+global.localStorage = {
+    store:{},
+    getItem: (key) => this.store[key],
+    setItem: (key, value) => this.store[key] = value
+ }
 
 
 CodigoDevicePost();
-
 test("call loginDevice()", () => {
     
 // TESTANDO SE CodigoDevicePost() EXECUTOU O MÉTODO loginDevice() CORRETAMENTE
+
     // TESTANDO SE document.getElementById() FOI EXECUTADO 8 VEZES (uma vez para cada elemento do vetor codeUser)
     expect(document.getElementById).toHaveBeenCalledTimes(8);
 
@@ -45,3 +54,13 @@ test("call loginDevice()", () => {
     expect(document.getElementById.mock.calls[6][0]).toBe('user-code-7')
     expect(document.getElementById.mock.calls[7][0]).toBe('user-code-8')
 });
+
+
+mockXHR.responseText = 'access_token=TOKEN_DE_ACESSO_GITHUB123&scope=repo%2Cuser&token_type=bearer'
+
+test("ConfirmaLoginContaUsuario()", () => {
+    ConfirmaLoginContaUsuario();
+
+    // TESTANDO SE O TOKEN DO GITHUB ESTÁ SENDO SALVO CORRETAMENTE
+    expect(localStorage.getItem('token')).toBe('TOKEN_DE_ACESSO_GITHUB123')
+})
