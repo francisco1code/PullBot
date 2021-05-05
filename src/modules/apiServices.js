25b84ad77feffe0abe5e397d358f09c34cbb1c31
LorrayneCardozo <lorraynecardozo15@gmail.com>
2021-05-05 18:45:11 -0300

    (#107) adaptações em métodos de criação de grafíco

--- src/modules/apiServices.js
+++ src/modules/apiServices.js
@@ -68,82 +68,78 @@ export function contribuinteRepositorio(numeroMilestone, token, $owner, $repo, s
 
   var xhr = new XMLHttpRequest();
   
-  
   xhr.addEventListener("readystatechange", function() {
-    if(this.readyState === 4) {
-     var recebeContribuintes = JSON.parse(this.responseText);
-    const $numerosContribuintes = recebeContribuintes.length;
-    milestone(numeroMilestone, recebeContribuintes, $numerosContribuintes, token, $owner, $repo, sprint);
-     
-    
+    if(xhr.readyState === 4) {
+
+      var recebeContribuintes = JSON.parse(xhr.responseText);
+      const $numerosContribuintes = recebeContribuintes.length;
+
+      milestone(numeroMilestone, recebeContribuintes, $numerosContribuintes, token, $owner, $repo, sprint);
   }});
   
   xhr.open("GET", "https://api.github.com/repos/"+$owner+"/"+$repo+"/contributors");
   xhr.setRequestHeader("authorization", "Bearer " + token);
   xhr.send();
-  
-  }
-  
-  function milestone(numeroMilestone, recebeContribuintes, numerosContribuintes, token, $owner, $repo, sprint){ 
-    var xhr = new XMLHttpRequest();
-  
-    xhr.addEventListener("readystatechange", function() {
-      if(this.readyState === 4) {
-        var b = String(this.responseText).split('url:')
-        
-       var resposta = JSON.parse(this.responseText);
-      
-       const resultadoProcura = milestoneDesejada(numeroMilestone,resposta, resposta.length)
-       
-       var nomeSprint = resultadoProcura.title
-        var dataAberturaMilestone =  resultadoProcura.created_at
-        var dataFechamentoMilestone = resultadoProcura.closed_at
-        console.log( resultadoProcura.created_at)
-       
-       ProcuraContribuicao(recebeContribuintes,  numerosContribuintes, dataAberturaMilestone, dataFechamentoMilestone, token,  $owner, $repo, nomeSprint);
-  
-      }
-    });
-    xhr.open("GET", "https://api.github.com/repos/"+$owner+"/"+$repo+"/milestones?state=all&sort=completeness");
-    xhr.setRequestHeader("accept", "application/vnd.github.v3+json");
-    xhr.setRequestHeader("authorization", "Bearer " + token);
-    xhr.send();
-  }
- 
-  function milestoneDesejada(numeroMilestone,resposta,  qtdMilestones){
-    
-    for(var i = 0; i < qtdMilestones; i++){
-      if (resposta[i].number == numeroMilestone){
-       
-        return resposta[i];
-      }
-    
-      
-    }
-  }
-     
-  function ProcuraContribuicao(recebeContribuintes, numerosContribuintes, dataAberturaMilestone, dataFechamentoMilestone, token, $owner, $repo, nomeSprint){
+}
   
 
+function milestone(numeroMilestone, recebeContribuintes, numerosContribuintes, token, $owner, $repo, sprint){ 
+  
+  var xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("readystatechange", function() {
+    if(xhr.readyState === 4) {
+      
+      var b = String(xhr.responseText).split('url:')
+      var resposta = JSON.parse(xhr.responseText);
+
+      const resultadoProcura = milestoneDesejada(numeroMilestone,resposta, resposta.length)
+
+      var nomeSprint = resultadoProcura.title
+      var dataAberturaMilestone =  resultadoProcura.created_at
+      var dataFechamentoMilestone = resultadoProcura.closed_at
+      
+      ProcuraContribuicao(recebeContribuintes,  numerosContribuintes, dataAberturaMilestone, dataFechamentoMilestone, token,  $owner, $repo, nomeSprint);
+
+    }
+  });
+  xhr.open("GET", "https://api.github.com/repos/"+$owner+"/"+$repo+"/milestones?state=all&sort=completeness");
+  xhr.setRequestHeader("accept", "application/vnd.github.v3+json");
+  xhr.setRequestHeader("authorization", "Bearer " + token);
+  xhr.send();
+}
+ 
+function milestoneDesejada(numeroMilestone,resposta,  qtdMilestones){
+  
+  for(var i = 0; i < qtdMilestones; i++){
+    if (resposta[i].number == numeroMilestone){
+
+      return resposta[i];
+    }
+  
+    
+  }
+}
+    
+function ProcuraContribuicao(recebeContribuintes, numerosContribuintes, dataAberturaMilestone, dataFechamentoMilestone, token, $owner, $repo, nomeSprint){
+  
   var i = 0;
   while(i < numerosContribuintes){
     var contribuinte = recebeContribuintes[i].login;
 
-  
     getContribuicao(contribuinte, numerosContribuintes,  dataAberturaMilestone, dataFechamentoMilestone, token, $owner, $repo, nomeSprint);
     i++;
   }
-  }
+}
   
-  function getContribuicao(contribuinte, numerosContribuintes, dataAberturaMilestone, dataFechamentoMilestone, token, $owner, $repo, nomeSprint){
-    
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener("readystatechange", function() {
-      if(this.readyState === 4) {
-        var todosCommits = JSON.parse(this.responseText);
-          
-         calculaCommits(contribuinte, todosCommits.length, numerosContribuintes, nomeSprint);
+function getContribuicao(contribuinte, numerosContribuintes, dataAberturaMilestone, dataFechamentoMilestone, token, $owner, $repo, nomeSprint){
+  
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("readystatechange", function() {
+    if(xhr.readyState === 4) {
       
+      var todosCommits = JSON.parse(xhr.responseText);
+        calculaCommits(contribuinte, todosCommits.length, numerosContribuintes, nomeSprint);
       }
     });
     
@@ -151,22 +147,19 @@ export function contribuinteRepositorio(numeroMilestone, token, $owner, $repo, s
     xhr.setRequestHeader("accept", "application/vnd.github.v3+json");
     xhr.setRequestHeader("authorization", "Bearer " + token);
     xhr.send();
-  }
+}
  
-  function calculaCommits(contribuinte, todosCommits , numerosContribuintes, nomeSprint){
-    
-    total = todosCommits + total;
+
+function calculaCommits(contribuinte, todosCommits , numerosContribuintes, nomeSprint){
   
-    nomeContribuinte[contador]  = contribuinte;
-    qtdComitsContribuinte[contador] = todosCommits;
-   
-   
-   if(contador == numerosContribuintes - 1){
-     console.log(nomeContribuinte)
-     console.log(qtdComitsContribuinte)
-     GraficoPessoal(nomeContribuinte, qtdComitsContribuinte, nomeSprint);
-   }
-    
-   contador++;
- 
+  total = todosCommits + total;
+  nomeContribuinte[contador]  = contribuinte;
+  qtdComitsContribuinte[contador] = todosCommits;
+  
+  if(contador == numerosContribuintes - 1){
+
+    GraficoPessoal(nomeContribuinte, qtdComitsContribuinte, nomeSprint);
   }
+  
+  contador++;
+}
