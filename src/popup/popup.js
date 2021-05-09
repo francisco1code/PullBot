35f848d6fa01fdfb4ef8b76fab298b4facb36340
ArthurMeloG <arthurdemelo2001@gmail.com>
2021-05-09 20:55:08 -0300

    (#119) Criando loader antes do grafico

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -16,48 +16,6 @@ chrome.storage.sync.get(['key'], function(result) {
   localStorage.setItem("token", token);
 });
 /*CREATE A DIV DISPLAY*/
-function CreateDivDisplay( DivAmount, numerDivPriority, start){
-
-	createClass('.plane0', "display: none ; z-index: -1;");
-	createClass('.plane1', "z-index: 1; display: grid !important;");
-	
-    for(var i = 1; i <=  DivAmount; i++){
-        if (i <= numerDivPriority & i >= start){
-            var DivPriority = eval(new String('div'+i))
-            applyClass('plane1', DivPriority);
-            applyClass('plane0',DivPriority, true);
-        }
-        else
-        {
-            var DivSecundary = eval(new String('div'+i))
-             applyClass('plane0',DivSecundary);
-             applyClass('plane1',DivSecundary, true);
-        }
-    }
-
-}
-
-function createClass(name,rules){
-	var style = document.createElement('style');
-	style.type = 'text/css';
-	document.getElementsByTagName('head')[0].appendChild(style);
-	if(!(style.sheet||{}).insertRule) 
-		(style.styleSheet || style.sheet).addRule(name, rules);
-	else
-		style.sheet.insertRule(name+"{"+rules+"}",0);
-}
-
-function applyClass(name,element,doRemove){
-	if(typeof element.valueOf() == "string"){
-		element = document.getElementById(element);
-	}
-	if(!element) return;
-	if(doRemove){
-		element.className = element.className.replace(new RegExp("\\b"+name+"\\b","g"),"");
-	}else{
-		element.className = element.className + " "+name;
-	}
-}
 
 const $div1 = document.getElementById('b1')
 const $div2 = document.getElementById('b2')
@@ -66,6 +24,7 @@ const $div4 = document.getElementById('b4')
 
 $div1.addEventListener('click', ()=> {
   
+  document.getElementById("div6").style.display = "grid"
   const owner = localStorage.getItem("owner")
   let repo = localStorage.getItem("repo")
   let numeroMilestone = localStorage.getItem("numeroMilestone")
